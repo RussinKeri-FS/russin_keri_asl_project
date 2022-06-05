87f298ff6a25708a637d1e21a668bc8e60c1ec53
Keri Russin <karussin@student.fullsail.edu>
2022-06-05 12:24:17 -0400

    starting fresh with new code

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
 const express = require("express");
 const app = express();
 
-// GET /products/all HTTP/1.1
-app.get('/', (req, res) => {
-    res.send('ASL Project! Keri Russin');
-});
 
-// GET /products/8719 HTTP/1.1
-app.get('/products/:id', (req, res) => {
-    res.send(`Product, ${req.params.id}`);
-    });
-
-// GET /products/8719-small-red HTTP/1.1 
-app.get('/products/:id-:size-:color', (req, res) => {
-    res.send(`Product: ${req.params.id}, ${req.params.size}, ${req.params.color}`);
-});
 
 
 app.listen(3000);
