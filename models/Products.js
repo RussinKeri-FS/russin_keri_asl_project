1d199065caa229cf79b930bc10061ab3edcf9d93
Keri Russin <karussin@student.fullsail.edu>
2022-06-12 14:58:59 -0400

    updated modles products.js

--- models/Products.js
+++ models/Products.js
@@ -29,7 +29,7 @@ const update = (id, product) => {
 };
 
 const remove = (id) => {
-  products = products.filter(p => p.id !== id);
+  products = products.filter((p) => p.id !== id);
   return products;
 };
 
